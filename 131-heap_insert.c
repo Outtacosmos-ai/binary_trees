d1d80f36818131a14890b08055fac3cbcbeaf692
Outtacosmos-ai <messghir4@gmail.com>
2024-12-23 15:22:37 +0100

    demons

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,47 +1,57 @@
 #include "binary_trees.h"
-
 /**
-	* swap - swaps two integers
-	* @a: first integer
-	* @b: second integer
-	*/
-void swap(int *a, int *b)
+ * swap_nodes - Swaps two nodes in a binary tree
+ * @a: First node
+ * @b: Second node
+ */
+void swap_nodes(heap_t *a, heap_t *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	int temp = a->n;
+
+	a->n = b->n;
+	b->n = temp;
 }
 
 /**
 	* binary_tree_size - measures the size of a binary tree
 	* @tree: pointer to the root node of the tree to measure the size
-	* Return: size of the tree, 0 if tree is NULL
+	* Return: Size of the tree, 0 if tree is NULL
 	*/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
-	return (0);
+		return (0);
 	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
 
 /**
-	* heapify_up - maintains the max heap property by moving a node up
-	* @node: pointer to the node to move up
+	* get_parent - finds the parent node for the new insertion
+	* @root: pointer to the root node
+	* @index: index where the new node should be inserted
+	* Return: pointer to the parent node
 	*/
-void heapify_up(heap_t *node)
+heap_t *get_parent(heap_t *root, size_t index)
 {
-	while (node->parent && node->n > node->parent->n)
+	size_t parent_idx = (index - 1) / 2;
+	size_t current = 0;
+	heap_t *parent = root;
+
+	while (current < parent_idx)
 	{
-	swap(&node->n, &node->parent->n);
-	node = node->parent;
+		if ((parent_idx - 1) / 2 >= current)
+			parent = parent->left;
+		else
+			parent = parent->right;
+		current = (current * 2) + 1;
 	}
+	return (parent);
 }
 
 /**
-	* heap_insert - inserts a value in Max Binary Heap
+	* heap_insert - inserts a value into a Max Binary Heap
 	* @root: double pointer to the root node of the Heap
 	* @value: value to store in the node to be inserted
-	* Return: pointer to the created node, or NULL on failure
+	* Return: pointer to the inserted node, or NULL on failure
 	*/
 heap_t *heap_insert(heap_t **root, int value)
 {
@@ -49,36 +59,31 @@ heap_t *heap_insert(heap_t **root, int value)
 	size_t size;
 
 	if (!root)
-	return (NULL);
+		return (NULL);
 
 	if (!*root)
 	{
-	*root = binary_tree_node(NULL, value);
-	return (*root);
+		*root = binary_tree_node(NULL, value);
+		return (*root);
 	}
 
 	size = binary_tree_size(*root);
-	parent = *root;
-
-	while (size > 1)
-	{
-	if (size % 2 == 0)
-	parent = parent->left;
-	else
-	parent = parent->right;
-	size /= 2;
-	}
+	parent = get_parent(*root, size);
 
 	new_node = binary_tree_node(parent, value);
 	if (!new_node)
-	return (NULL);
+		return (NULL);
 
-	if (!parent->left)
-	parent->left = new_node;
+	if (size % 2)
+		parent->right = new_node;
 	else
-	parent->right = new_node;
+		parent->left = new_node;
 
-	heapify_up(new_node);
+	while (new_node->parent && new_node->n > new_node->parent->n)
+	{
+		swap_nodes(new_node, new_node->parent);
+		new_node = new_node->parent;
+	}
 
 	return (new_node);
 }
